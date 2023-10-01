7589828077d62d7f9a96030b2d41dedadfbb0031
Ambuj-kumar-rai <61017620+Ambuj-kumar-rai@users.noreply.github.com>
2023-10-01 18:50:16 +0000

    we learned dataTypes in js

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,33 @@
+"use strict" // treat all js code as newer version
+
+// alert("Hello")
+// alert(3+4) // we are using node js, not browser hence we are getting error in node js
+
+console.log("Ambuj"); console.log("Rai") // as per the readability this line is not a good practice, below line is good practice(we can start new line for readibility)
+
+console.log("Ambuj")
+console.log(5+10)
+
+let name = "Ambuj"
+let age = 23
+let isLoggedIn = false
+
+
+// primitive data types are :
+
+
+    // Number => range is, 2 to power 52
+    //bigint it is used to store large number
+    // string =. ""
+    //boolean => true/false
+    //null  => standlone value (repersentation of empty value)
+    // undefined => when value is not assigned to any variable that type is undefined
+    //symbol => unique
+
+console.log(typeof "anc") // (typeof) it will return the type of value
+console.log(typeof age)
+
+console.log(typeof undefined) // undefined 
+console.log(typeof null) // object
+
+// note: in js datatype "null" is an object 

cf8e656ca247119f5570ba3bdc07900053dd8ccd
Ambuj-kumar-rai <61017620+Ambuj-kumar-rai@users.noreply.github.com>
2023-10-02 07:24:58 +0000

    conversion operation from one datatype to another dataType

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -39,3 +39,43 @@ console.log(convertValue2) // here it will also return the NaN value, if we will
 //"hitesh" => true --> string to boolean
 // "" => false --> empty string to boolean
 // "33" => 33 --> string to num
+
+
+// *************************************OPERATIONS*******************************************************
+
+let value = 5
+let negValue = -value
+console.log(negValue)
+
+
+// console.log(2+2);
+// console.log(22-12);
+// console.log(2*2);
+// console.log(2**3); exponential
+// console.log(5/2);
+// console.log(5%2); reminder
+
+let str1 = "hello"
+let str2 = " Ambuj"
+let str3 = (str1+str2)
+console.log(str3) //it will return hello Ambuj(concatination of string)
+
+
+console.log("1" + 2) // 12
+console.log(1 + 2) //3
+console.log("1" + 2 + 2) //122
+console.log(1 + 2 + "2") // 32
+console.log(true) // true
+console.log(+true) //1
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+
+// note, Above code pattern is not a good practice to write code.
+
+
+
+// note: If the first operand is string then the conversion will be in string
+// note: If the first operand is Number then contiguous number will do aritmatic opration and will concatiante with the followed string
+
